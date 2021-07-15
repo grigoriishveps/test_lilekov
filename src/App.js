314be603d53dfdec154b717792f36551c8454c04
grigoriishveps <grigorii.shveps@gmail.com>
2021-07-16 00:53:22 +0300

    ready test project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import  {Redirect,Route, Switch} from "react-router-dom";
+import ContentPage from "./components/ContentPage";
+import AuthComponent from "./components/AuthComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App-header">
+          <Switch>
+              <Route path='/login' component={AuthComponent}/>
+              <Route path='/' exact component={ContentPage}/>
+              <Route path='*'> <Redirect to="/login"/></Route>
+          </Switch>
+        </div>
     </div>
   );
 }
